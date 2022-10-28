7bef9ea3663e82043560f529362b725479bd7f5e
Gino <35615@ma-web.nl>
2022-10-28 16:03:43 +0200

    wat ik tot nu toe heb

---
+++ BO.py
@@ -0,0 +1,120 @@
+from time import sleep
+
+
+
+
+def vraag1():
+    print("Jouw wereld is in groot gevaar door het klimaat en een zwart gat.")  
+    print ("")     
+     = input()   
+    if  ==("a"):         
+        print ("")   
+    elif    ==("b"):         
+        print ("")     
+    elif     ==("c"):
+        print ("")    
+    else:         
+        print ("Not a option")         
+        voorbeeldq()
+
+def vraag2():
+    print("Doe niks en wacht maar de raket staat wel klaar")
+    sleep(1)
+    print("2: wat ga je doen, A: de raket in of B: thuis blijven")    
+    
+    print("")    
+
+def vraag3():
+    print("")
+
+
+
+def vraag4():
+    print("")
+
+
+def vraag5():
+    print("")
+
+
+def vraag6():
+    print("")
+
+
+def vraag7():
+    print("")
+
+
+def vraag8():
+    print("")
+
+
+def vraag9():
+    print("")
+
+
+def vraag10():
+    print("")
+
+
+def vraag11():
+    print("")
+
+
+def vraag12():
+    print("")
+
+
+def vraag13():
+    print("")
+
+
+def vraag14():
+    print("")
+
+
+def vraag15():
+    print("")
+
+
+def vraag16():
+    print("")
+
+
+def vraag17():
+    print("")
+
+
+def vraag18():
+    print("")
+
+
+def vraag19():
+    print("")
+
+
+def vraag20():
+    print("")
+
+
+def vraag21():
+    print("")
+
+
+def Einde1():
+    print("")
+
+
+def Einde2():
+    print("")
+
+
+def Einde3():
+    print("")    
+
+
+def Einde4():
+    print("")
+
+
+
